968706a247fb262830f654361c0c5dac3084fc52
Youssef AbdelMeguid <yousef.dev.c@gmail.com>
2021-06-23 16:51:04 +0200

    Integrate google mercahnt with .net, ContentAPI

---
+++ GoogleMerchant.cs
@@ -0,0 +1,194 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Requests;
+using Google.Apis.Services;
+using Google.Apis.ShoppingContent.v2_1;
+using Google.Apis.ShoppingContent.v2_1.Data;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleMerchant
+{
+    public class GoogleMerchant
+    {
+
+        private UserCredential Credential;
+
+        private ShoppingContentService Service;
+
+        public ulong MerchantId { get; set; }
+
+        public GoogleMerchant()
+        {
+
+        }
+
+        public GoogleMerchant(string clientId, string clientSecret, string applicationName, ulong merchantId)
+        {
+
+            MerchantId = merchantId;
+
+            Credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets() 
+                { 
+                    ClientId = clientId, 
+                    ClientSecret = clientSecret 
+                },
+                new string[] { ShoppingContentService.Scope.Content },
+                "user",
+                CancellationToken.None
+                ).Result;
+
+            Service = new ShoppingContentService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = Credential,
+                ApplicationName = applicationName
+            });
+
+        }
+
+        public async Task InsertProductBatch(List<Product> products)
+        {
+            BatchRequest request = new BatchRequest(Service);
+
+            foreach (var p in products)
+            {
+                ProductsResource.InsertRequest insertRequest = Service.Products.Insert(p, MerchantId);
+
+                request.Queue<Product>(insertRequest, (content, error, index, message) => 
+                {
+                    if(content != null)
+                        Console.WriteLine(String.Format("Product inserted with id {0}", ((Product)content).Id));
+
+                    if(error != null)
+                        Console.WriteLine(error.Message);
+
+                    if(message != null)
+                        Console.WriteLine(message.StatusCode);
+                });
+            }
+
+            await request.ExecuteAsync(CancellationToken.None);
+
+        }
+
+        public async Task<bool> InsertProduct(Product product)
+        {
+            ProductsResource.InsertRequest insertRequest = Service.Products.Insert(product, MerchantId);
+            bool isInserted = false;
+            try
+            {
+                var insertedProduct = await insertRequest.ExecuteAsync(CancellationToken.None);
+                isInserted = true;
+                Console.WriteLine(string.Format("Product inserted with id {0}", insertedProduct.Id));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+
+            return isInserted;
+
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+
+            bool isDelete = false;
+
+            ProductsResource.DeleteRequest deleteRequest = Service.Products.Delete(MerchantId, id);
+
+            try
+            {
+                await deleteRequest.ExecuteAsync(CancellationToken.None);
+                isDelete = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+
+            return isDelete;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            ProductsResource.GetRequest getRequest = Service.Products.Get(MerchantId, id);
+            Product product = new Product();
+
+            try
+            {
+                product = await getRequest.ExecuteAsync(CancellationToken.None);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+
+            return product;
+        }
+
+        public async Task<List<Product>> GetProducts()
+        {
+            ProductsResource.ListRequest listRequest = Service.Products.List(MerchantId);
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                ProductsListResponse response = await listRequest.ExecuteAsync(CancellationToken.None);
+                foreach (var p in response.Resources)
+                    products.Add(p);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+
+            return products;
+        }
+
+        public async Task<bool> UpdateProduct(Product product, string id)
+        {
+            ProductsResource.UpdateRequest updateRequest = Service.Products.Update(product, MerchantId, id);
+            bool isUpdated = false;
+            try
+            {
+                var updatedProducted = await updateRequest.ExecuteAsync(CancellationToken.None);
+                Console.WriteLine("producte with id {0} has been updated", updatedProducted.Id);
+                isUpdated = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+
+            return isUpdated;
+
+        }
+
+        public async Task<bool> CreateOrder(OrdersCreateTestOrderRequest order)
+        {
+            bool isOrderCreated = false;
+
+            OrdersResource.CreatetestorderRequest request = Service.Orders.Createtestorder(order, MerchantId);
+
+            try
+            {
+                OrdersCreateTestOrderResponse response = await request.ExecuteAsync(CancellationToken.None);
+                isOrderCreated = true;
+                Console.WriteLine("Order created with id {0}", response.OrderId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+
+
+            return isOrderCreated;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,189 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Requests;
+using Google.Apis.Services;
+using Google.Apis.ShoppingContent.v2_1;
+using Google.Apis.ShoppingContent.v2_1.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using GoogleMerchant;
+using System.Configuration;
+
+namespace GoogleMerchant
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+
+            string clientId = ConfigurationManager.AppSettings["ClientID"].ToString();
+            string clientSecret = ConfigurationManager.AppSettings["ClientSecret"].ToString();
+            string appName = ConfigurationManager.AppSettings["ApplicationName"].ToString();
+            ulong merchantId = ulong.Parse(ConfigurationManager.AppSettings["MerchantID"].ToString());
+
+            GoogleMerchant googleMerchant = new GoogleMerchant(
+                clientId,
+                clientSecret, 
+                appName, 
+                merchantId);
+
+
+            List<string> fullAddress = new List<string>();
+            fullAddress.Add("Youssef");
+            fullAddress.Add("AbdelMeguid");
+            fullAddress.Add("3");
+            fullAddress.Add("haha");
+            fullAddress.Add("ainshams");
+            fullAddress.Add("cairo");
+            fullAddress.Add("egypt");
+
+            List<TestOrderPickupDetailsPickupPerson> p = new List<TestOrderPickupDetailsPickupPerson>();
+            p.Add(new TestOrderPickupDetailsPickupPerson()
+            {
+                Name = "Youssef AbdelMeguid",
+                PhoneNumber = "+201555954750"
+            });
+
+            List<TestOrderLineItem> items = new List<TestOrderLineItem>();
+            items.Add(new TestOrderLineItem()
+            {
+                QuantityOrdered = 1,
+                ShippingDetails = new OrderLineItemShippingDetails()
+                {
+                    Type = "pickup",
+                    DeliverByDate = "2021-07-16T19:20:30+01:00",
+                    ShipByDate = "2021-07-16T19:20:30+01:00",
+                    Method = new OrderLineItemShippingDetailsMethod()
+                    {
+                        MaxDaysInTransit = 12,
+                        MinDaysInTransit = 8,
+                        MethodName = "otlob",
+                        Carrier = "otlob"
+                    }
+                },
+                ReturnInfo = new OrderLineItemReturnInfo()
+                {
+                    DaysToReturn = 0,
+                    IsReturnable = false,
+                    PolicyUrl = ""
+                },
+                Product = new TestOrderLineItemProduct()
+                {
+                    Brand = "nity",
+                    Condition = "new",
+                    ContentLanguage = "en",
+                    OfferId = "2",
+                    Price = new Price()
+                    {
+                        Currency = "EGP",
+                        Value = "122.0"
+                    },
+                    TargetCountry = "EG",
+                    Title = "catsss",
+                    ImageLink = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTYxr4iHCIhk5UBruy4B3jG-AjcWK-GDYQZabs-UTqoxjFJdrU0RVJBVfQ8BS-Ck3BJaqtqPJvPgw"
+                }
+            });
+
+            OrdersCreateTestOrderRequest order = new OrdersCreateTestOrderRequest()
+            {
+                Country = "Egypt",
+                TemplateName = "template1",
+                TestOrder = new TestOrder()
+                {
+                    PredefinedDeliveryAddress = "jim",
+                    PredefinedBillingAddress = "jim",
+                    PredefinedEmail = "pog.dwight.schrute@gmail.com",
+                    DeliveryDetails = new TestOrderDeliveryDetails()
+                    {
+                        Address = new TestOrderAddress() 
+                        {
+                            Country = "Egypt",
+                            FullAddress = fullAddress,
+                            IsPostOfficeBox = false,
+                            Locality = "ainshams",
+                            PostalCode = "113322",
+                            RecipientName = "Youssef AbdelMeguid",
+                            Region = "cairo",
+                            StreetAddress = fullAddress
+                        },
+                        IsScheduledDelivery = true,
+                        PhoneNumber = "+201555954750"
+                    },
+                    PredefinedPickupDetails = "jim",
+                    ShippingCost = new Price()
+                    {
+                        Value = "1002.2",
+                        Currency = "EGP"
+                    },
+                    ShippingOption = "standard",
+                    PickupDetails = new TestOrderPickupDetails()
+                    {
+                        LocationCode = "033",
+                        PickupLocationType = "store",
+                        PickupLocationAddress = new TestOrderAddress()
+                        {
+                            Country = "Egypt",
+                            FullAddress = fullAddress,
+                            IsPostOfficeBox = false,
+                            Locality = "ainshams",
+                            PostalCode = "113322",
+                            RecipientName = "Youssef AbdelMeguid",
+                            Region = "cairo",
+                            StreetAddress = fullAddress
+                        },
+                        PickupPersons = p
+                    },
+                    EnableOrderinvoices = false,
+                    LineItems = items
+                }
+            };
+
+            var createOrder = await googleMerchant.CreateOrder(order);
+
+            var product = await googleMerchant.GetProducts();
+
+            foreach (var po in product)
+            {
+                Console.WriteLine(po.Id);
+            }
+
+        }
+
+    }
+}
+
+
+
+
+//List<Product> products = new();
+//products.Add(new Product()
+//{
+//    Title = "catsss"
+
+//});
+
+
+//var update = await googleMerchant.UpdateProduct(products[0], "online:en:EG:2");
+//Console.WriteLine(update.ToString());
+
+//await googleMerchant.InsertProduct(products[0]);
+
+//googleMerchant.InsertProductBatch(products).Wait();
+
+//var d = await googleMerchant.DeleteProduct("online:en:EG:2");
+
+//Console.WriteLine(d.ToString());
+
+//var products = await googleMerchant.GetProducts();
+
+//foreach (var p in products)
+//    Console.WriteLine(p.Id);
+
+//var product = await googleMerchant.GetProducts();
+
+//foreach (var p in product)
+//{
+//    Console.WriteLine(p.Id);
+//}
